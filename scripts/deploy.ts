41f22cdb2497691fb7a5b182760e10048b2e4654
Jamie Hewitt <jamie.hewitt15@gmail.com>
2025-04-23 10:53:52 +0300

    Implementing ERC20 token contract

---
+++ scripts/deploy.ts
@@ -0,0 +1,14 @@
+// import { ethers } from 'hardhat'
+
+async function main() {
+  //   const VotingFactory = await ethers.getContractFactory('Voting')
+  //   const voting = await VotingFactory.deploy()
+  //   const votingContract = await voting.deployed()
+  //   console.log('Voting contract deployed:', votingContract)
+  //   console.log('Voting deployed to address:', votingContract.address)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
