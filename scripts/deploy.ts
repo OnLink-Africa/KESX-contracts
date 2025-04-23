613c94be0ca03cb20c56cd1e8d41fd593535b960
Jamie Hewitt <jamie.hewitt15@gmail.com>
2025-04-23 10:57:03 +0300

    Setting up hardhat

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,26 @@
-// import { ethers } from 'hardhat'
+import { ethers } from 'hardhat'
 
 async function main() {
-  //   const VotingFactory = await ethers.getContractFactory('Voting')
-  //   const voting = await VotingFactory.deploy()
-  //   const votingContract = await voting.deployed()
-  //   console.log('Voting contract deployed:', votingContract)
-  //   console.log('Voting deployed to address:', votingContract.address)
+  const [deployer] = await ethers.getSigners()
+  console.log('Deploying contracts with the account:', deployer.address)
+
+  // Token parameters
+  const name = 'My Token'
+  const symbol = 'MTK'
+  const initialSupply = 1000000 // 1 million tokens
+
+  // Deploy the token
+  const TokenFactory = await ethers.getContractFactory('MyToken')
+  const token = await TokenFactory.deploy(name, symbol, initialSupply)
+
+  console.log('Token deployed to:', await token.getAddress())
+  console.log('Token name:', await token.name())
+  console.log('Token symbol:', await token.symbol())
+  console.log('Total supply:', ethers.formatEther(await token.totalSupply()))
+  console.log(
+    'Deployer balance:',
+    ethers.formatEther(await token.balanceOf(deployer.address))
+  )
 }
 
 main().catch((error) => {
