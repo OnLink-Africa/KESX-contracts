1d5c8f6fe6d9809770da3bf4b442fe7a94e1dbe9
Jamie Hewitt <jamie.hewitt15@gmail.com>
2025-04-23 12:12:40 +0300

    Removing unused files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,39 @@ import '@nomicfoundation/hardhat-toolbox'
 import '@nomicfoundation/hardhat-ignition'
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.29'
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.29',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: '0.4.24', // For USDC contracts
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    ],
+    overrides: {
+      'contracts/mockUSDC.sol': {
+        version: '0.4.24',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    }
+  }
 }
 
 export default config
