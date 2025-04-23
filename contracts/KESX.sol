d06d9fcc9c83805bbeadfdcb3272f0b28f1ff5d1
Jamie Hewitt <jamie.hewitt15@gmail.com>
2025-04-23 12:34:20 +0300

    Updating contract to allow burning and minting

--- contracts/KESX.sol
+++ contracts/KESX.sol
@@ -2,9 +2,20 @@
 pragma solidity ^0.8.29;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ERC20Token is ERC20 {
-    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+contract ERC20Token is ERC20, ERC20Burnable, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) Ownable(msg.sender) {
         _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+
+    /**
+     * @dev Allows the owner to mint new tokens
+     * @param to The address that will receive the minted tokens
+     * @param amount The amount of tokens to mint
+     */
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
